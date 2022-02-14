4bcefc3bdd8e36b1562a27680c025deffbf32d67
love kalsangrah <love.kalsangrah@gmail.com>
2022-02-14 11:05:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import TableShow from './components/TableShow';
+import Home from './components/Home';
+import AddEmployee from './components/AddEmployee'
+// import Login from './components/Login';
+// import Registration from './components/Registration';
+import {  Container, Nav, Navbar, Table } from 'react-bootstrap';
+import { useState } from 'react';
+import { ContextProvider } from './context/context';
+import EditEmployee from './components/EditEmployee';
+
+
+function App() {
+  const[storeData,setStoreData]=useState([])
+  const [showEditModal,setEditModal]=useState(false)
+  const [showLogin,setShowLogin]=useState(true)
+  const[selectedEmployee,setSelectedEmployee]=useState({
+    fullName:'',
+    phonenumber:'',
+    emial:''
+    
+  })
+const [index,setIndex]=useState(0)
+  const changeLogin=()=>{
+    setShowLogin(true)
+  }
+  return (
+   
+      <Router>
+      <div className='App'>
+        <Navbar className='navbar navbar-expand-lg navbar-primary bg-warning'>
+          <Container>
+            <Navbar.Brand style={{color:'white'}}>LUV KALSANGRAH</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              {/* <Nav className="me-auto">
+               {showLogin ? <div><Link to="/login">Login</Link ><Link to='/registration'>Register</Link></div>: <div style={{display:'flex'}}><div><Link to="/tableshow">Employees</Link>
+                <Link to="/add-employee" style={{marginLeft:'15px'}}>Add Employee</Link></div><div><Link to='/login' onClick={()=>{changeLogin()}}>LogOut</Link></div></div>}
+              </Nav> */}
+
+              <div >
+                <Link to="/add-employee">Add-employee</Link> </div>
+               <div style={{position:"relative",left:"15px"}}> <Link to="/tableshow">Table-data</Link></div>
+
+             
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div>
+        <ContextProvider value={{storeData,setStoreData,showEditModal,setEditModal,setShowLogin,selectedEmployee,setSelectedEmployee,setIndex,index}}>
+            <Route exact={true} path="/" component={Home} />
+            <Route path="/tableshow" component={TableShow} />
+            <Route path="/add-employee" component={AddEmployee} />
+            {/* <Route path='/login' component={Login}/> */}
+            {/* <Route path='/registration' component={Registration}/> */}
+            <Route path='/editEmployee' component={EditEmployee} />
+            </ContextProvider>
+        </div>
+      </div>
+    </Router>
+   
+
+
+  );
+}
+
+export default App;
---
+++ src/components/AddEmployee.js
@@ -0,0 +1,152 @@
+import React,{ useContext, useEffect, useState } from 'react';
+import ContextData from '../context/context';
+import EditEmployee from './EditEmployee';
+
+function AddEmployee() {
+   const recivedData=useContext(ContextData)
+    const [employeeDetails, setEmployeeDetails] = useState({
+        fullName:'',
+        phonenumber:'',
+        email:''
+      
+    });
+
+    const [nameErrorMessage, setNameErrorMessage] = useState('');
+    const [ageErrorMessage, setAgeErrorMessage] = useState('');
+    const [salaryErrorMessage, setSalaryErrorMessage] = useState('');
+
+    const [nameError, setnameError] = useState(false);
+    const [designationError, setdesignationError] = useState(false);
+    const [salaryError, setSalaryError] = useState(false);
+    const [ageError, setAgeError] = useState(false);
+
+    const handleSubmit=(event)=>{
+        event.preventDefault()
+        const isNameValid=validateName(employeeDetails.fullName)
+        const isDesignationValid=validateDesignation(employeeDetails.phonenumber)
+        const isSalaryValid=validateSalary(employeeDetails.email)
+        const isAgeValid=validateAge(employeeDetails.age)
+
+        if (true){
+          const array= [...recivedData.storeData]
+          array.push(employeeDetails)
+          recivedData.setStoreData(array)
+            console.log(recivedData.storeData);
+            setEmployeeDetails({
+                fullName:'',
+                phonenumber:'',
+                email:''
+               
+            })
+            alert('Details Added Successfully')
+        }else {
+            console.error('invalid data');
+        }
+    }
+//===========================================================================
+    const validateName=(name)=>{
+        // const nameFormat= /[a-zA-Z]+/
+        // if (name===''){
+        //     setnameError(true)
+        //     setNameErrorMessage('Please enter username')
+        //     return false
+        // }else if (nameFormat.test(name)){
+        //     setnameError(false)
+        //     setNameErrorMessage(' ')
+        //     return true
+        // } else{
+        //     setnameError(true)
+        //     setNameErrorMessage('Please enter valid username')
+        //     return false
+        // }
+    }
+
+    const validateDesignation=(designation)=>{
+        // if (designation){
+        //     setdesignationError(false)
+        //     return true
+        // }else{
+        //     setdesignationError(true)
+        //     return false
+        // }
+    }
+
+    const validateSalary=(salary)=>{
+        // if (salary===''){
+        //     setSalaryError(true)
+        //     setSalaryErrorMessage('please enter salary')
+        //     return true
+        // }
+        // if (salary>0){
+        //     setSalaryError(false)
+        //     setSalaryErrorMessage(' ')
+        //     return true
+        // }
+        // else{
+        //     setSalaryError(true)
+        //     setSalaryErrorMessage('')
+        //     return true
+        // }
+    }
+
+    const validateAge=(age)=>{
+        // if (age===''){
+        //     setAgeError(true)
+        //     setAgeErrorMessage('Please Enter Age')
+        //     return false
+        // }else if (age>18){
+        //     setAgeError(false)
+        //     setAgeErrorMessage('')
+        //     return true
+        // }else{
+        //     setAgeError(true)
+        //     setAgeErrorMessage('Age not valid')
+        //     return false
+        // }
+    }
+//=================================================================================
+    const handleData=(event)=>{
+        setEmployeeDetails({
+            ...employeeDetails,
+            [event.target.name]:event.target.value
+        })
+    }
+
+  return <div class='division'>
+        <form class="row g-2 form shadow rounded " style={{width:'500px', marginLeft:'450px', marginTop:'20px'}}onSubmit={(event)=>{handleSubmit(event)}}>
+            <div class="form-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add Employee Details</h5>
+            </div>
+            <div class="form-div">
+                <label class="form-label">Full Name</label>
+                <input type="text" class="form-control" placeholder='Enter full name' onChange={(e)=>{handleData(e)}} value={employeeDetails.fullName} name='fullName'/>
+                {nameError ? <div class="error-message">{nameErrorMessage}</div> : null}
+            </div>
+
+            <div class="">
+                <label class="form-label">Phone Number</label>
+                <input type="number" class="form-control" placeholder='Phone Number' onChange={(e)=>{handleData(e)}} value={employeeDetails.phonenumber} name='phonenumber'/>
+                {designationError ? <span class="error-message">Please choose a designation.</span> : null }
+                
+            </div>
+
+            <div class="">
+                <label class="form-label">Email</label>
+                <input type="email" class="form-control" placeholder='Email' onChange={(e)=>{handleData(e)}} value={employeeDetails.email} name='email'/>
+                {salaryError ? <span class="error-message">{salaryErrorMessage}</span> : null }
+            </div>
+
+            {/* <div class="">
+                <label class="form-label">Age</label>
+                <input type="number" class="form-control" placeholder='Enter age' onChange={(e)=>{handleData(e)}} value={employeeDetails.age} name='age'/>
+                {ageError ? <span class="error-message">{ageErrorMessage}</span> : null }
+            </div> */}
+
+            <div class="button">
+                <button  class="btn btn-primary " type="submit">Submit </button>
+            </div>
+        </form>
+  </div>;
+}
+
+export default AddEmployee
